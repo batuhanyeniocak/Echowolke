8a87f4ee7c66ad721735471a5af562de1bdf37c0
batuhanyeniocak <nxxbaxxu@icloud.com>
2025-06-03 20:19:03 +0300

    firebase servisi icin servis dosyasi olusturuldu
    firebase servisi icin servis dosyasi olusturuldu

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,9 @@ import Foundation
 
 import audio_service
 import audio_session
+import cloud_firestore
 import firebase_core
+import firebase_storage
 import just_audio
 import path_provider_foundation
 import shared_preferences_foundation
@@ -16,7 +18,9 @@ import sqflite_darwin
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
